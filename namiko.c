a5d0c14045f6f0290ac5c89cb08991822ebd0273
Jess-lnga <184637251+Jess-lnga@users.noreply.github.com>
2026-02-01 23:59:23 +0100

    Added some smooth transitions

--- namiko.c
+++ namiko.c
@@ -14,10 +14,56 @@ static void core1_entry(void) {
     wake_up();
     int mode = 1;
 
+    bool incr_angle = true;
+    bool modif_angle = true;
+    bool back_to_zero_angle = false;
+
+    bool incr_dist = true;
+    bool modif_dist = true;
+    
+
+    int angle = 0;
+    int step_angle = 2;
+
+
+    float D = 0;
+    float step_d = 0.25;
+
+    float D_max = 4;
+    float D_min = 0;
+
+    float Angle_max = 20;
+    float Angle_min = -20;
+
 
     while (true) {
-        //demo(mode);   
-        move(0, 0, 10*M_PI/180);
+        //demo(mode); 
+        
+        for(int i = 0; i < 1; ++i){
+            move(D, 0, angle*M_PI/180);
+        }
+
+        if(modif_dist){
+            if(incr_dist){
+                D += step_d;
+                if(D > D_max){D -= 2*step_d; incr_dist = false;}
+            }else{
+                D -= step_d;
+                if(D < D_min){D = D_min; incr_dist = true; modif_dist = false;}
+            }
+        }else{
+            if(incr_angle){
+                angle += step_angle;
+
+                if(angle > Angle_max){angle -= 2*step_angle; incr_angle = false;}
+                if((back_to_zero_angle)&&(angle > 0)){angle = 0; modif_dist = true; back_to_zero_angle = false;}
+
+            }else{
+                angle -= step_angle;
+                if(angle < Angle_min){angle = Angle_min; incr_angle = true; back_to_zero_angle = true;}
+            }
+
+        }
     }
 }
 
