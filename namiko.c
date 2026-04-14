0dc98a3f4a09e6862cccf644688722e76e6a65ab
Jess-lnga <184637251+Jess-lnga@users.noreply.github.com>
2026-04-14 07:30:22 +0200

    Don't look at this

--- namiko.c
+++ namiko.c
@@ -2,7 +2,7 @@
 // Author: Jérôme ESSOLA ELANGA - jerome.essolaelanga@epfl.ch
 // Team members: Jérôme ESSOLA ELANGA
 
-
+/*
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
@@ -19,35 +19,37 @@ static volatile bool g_line_following_ready = false;
 static volatile bool g_tof_ready = false;
 
 static void core1_entry(void) {
-    //init_servo_ctrl();
+    init_servo_ctrl();
 
-    //wake_up();
+    wake_up();
     sleep_ms(2000);
 
     int angle_deg = 0;
     int increment = 0;
 
     while(true){
-        //follow_line_step();
-        if (!g_tof_ready) {
-            printf("TOF init failed\n");
-            sleep_ms(500);
-            continue;
-        }
+        follow_line_step();
+        
+        //if (!g_tof_ready) {
+        //    printf("TOF init failed\n");
+        //    sleep_ms(500);
+        //    continue;
+        //}
 
-        int d1 = get_dist_1();
-        int d2 = get_dist_2();
-        int d3 = get_dist_3();
+        //int d1 = get_dist_1();
+        //int d2 = get_dist_2();
+        //int d3 = get_dist_3();
 
         //printf("\033[H\033[J");
         //printf("TOF distances: d1=%d mm | d2=%d mm | d3=%d mm\n", d1, d2, d3);
-        printf("\033[1GTOF distances: d1=%d mm | d2=%d mm | d3=%d mm    ", d1, d2, d3);
-        fflush(stdout);
+        //printf("\033[1GTOF distances: d1=%d mm | d2=%d mm | d3=%d mm    ", d1, d2, d3);
+        //fflush(stdout);
 
 
 
 
-        sleep_ms(100);
+        //sleep_ms(100);
+        
     }
 }
 
@@ -75,11 +77,13 @@ int main() {
     static uint16_t frame[OV7670_IMG_WIDTH * OV7670_IMG_HEIGHT];
 
     while (true) {
-        //ov7670_capture_frame(frame);
-        //find_line_pos(frame, OV7670_IMG_WIDTH, OV7670_IMG_HEIGHT);
-        //ov7670_send_frame_usb(frame);
+        ov7670_capture_frame(frame);
+        find_line_pos(frame, OV7670_IMG_WIDTH, OV7670_IMG_HEIGHT);
+        ov7670_send_frame_usb(frame);
     
         
         sleep_ms(1);
     }
 }
+
+*/
